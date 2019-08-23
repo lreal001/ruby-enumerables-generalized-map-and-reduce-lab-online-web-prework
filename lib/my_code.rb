b5b894c7207924e573a86993f017a850f9af2e37
Lessllie Real <rlessllie@yahoo.com>
2019-08-23 01:39:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,42 @@
 # Your Code Here
+def 
+  map([1,2,3,-9]){|n| n*-1}
+end 
+
+def 
+  map(["paul", "gurney", "vladimir", "jessica", "chani"])
+  
+end
+
+def 
+  map([1,2,3,-9]) {|n| n*2}
+end 
+
+
+def
+map([1,2,3,-9]){|n|n**2 }
+end
+
+def
+reduce([1,2,3])(0, :+)
+end
+
+def
+reduce([1,2,3](0, :+)
+end 
+
+def
+reduce([ false, nil, nil, nil, true]).any?
+end 
+
+def
+reduce([1, 2, true, "razmatazz"]).all?
+end
+
+def 
+  reduce([ false, nil, nil, nil, true]).any?
+end 
+
+def 
+  reduce([ false, nil, nil, nil]).any?
+end
